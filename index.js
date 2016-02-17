7ef40e2ef691065adeeaa2c762f8305dddc668f5
JonathanAndrew <jonandrewstevenson@yahoo.com>
2016-02-17 15:03:59 -0800

    set up session

--- index.js
+++ index.js
@@ -2,16 +2,25 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var ejsLayouts = require('express-ejs-layouts');
 var app = express();
+var session = require('express-session');
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(ejsLayouts);
+app.use(session({
+	secret : 'lkgfhgklgklfsldflksdfasd',
+	resave : false,
+	saveUninitialized : true
+}));
+
 
 app.get('/', function(req, res) {
+	console.log(req.session.animal);
   res.render('index');
 });
 
 app.get('/secret', function(req, res) {
+	req.session.animal = 'dog';
   res.render('secret');
 });
 
